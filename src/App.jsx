1a443537ca883978241d3a2c1024882bf1cb97b4
Sat Paing Thu <s-paingthu@isgm2.com>
2023-04-05 08:57:54 +0630

    feat: add remove feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,61 @@ import Pie from "./components/Pie";
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 const origianlLayouts = JSON.parse(localStorage.getItem("layouts")) || "{}";
-
+const originalItems = JSON.parse(localStorage.getItem("items")) || [
+  { i: "1", x: 0, y: 0, w: 5, h: 3 },
+  { i: "2", x: 5, y: 0, w: 4, h: 3 },
+  { i: "3", x: 9, y: 0, w: 3, h: 3 },
+];
 function App() {
   const [layouts, setLayouts] = useState(origianlLayouts);
   const [isEditable, setIsEditable] = useState(false);
-
-  const items = [
-    { i: "1", x: 0, y: 0, w: 5, h: 3 },
-    { i: "2", x: 5, y: 0, w: 4, h: 3 },
-    { i: "3", x: 9, y: 0, w: 3, h: 3 },
-  ];
+  const [items, setItems] = useState(originalItems);
   function handleEditable() {
     setIsEditable((prev) => !prev);
   }
   function handleSave() {
-    alert("Layouts saved in LocalStorage");
+    alert("Layouts & Items saved in LocalStorage");
     localStorage.setItem("layouts", JSON.stringify(layouts));
+    localStorage.setItem("items", JSON.stringify(items));
   }
   function handleLayoutChange(layout, layouts) {
     setLayouts(layouts);
   }
   function handleReset() {
-    setLayouts([]);
     localStorage.clear();
+    setTimeout(() => {
+      setLayouts([]);
+      setItems(originalItems);
+    }, 100);
   }
+  function handleRemove(i) {
+    setItems((items) => items.filter((item) => item.i !== i));
+  }
+
   return (
     <div>
-      <button onClick={handleEditable} style={{ margin: 10 }}>
+      <button
+        onClick={handleEditable}
+        className="btn btn-primary btn-sm"
+        style={{ margin: 10 }}
+      >
         Editable
       </button>
-      <button onClick={handleSave} style={{ margin: 10 }}>
+      <button
+        onClick={handleSave}
+        className="btn btn-success btn-sm"
+        style={{ margin: 10 }}
+      >
         Save
       </button>
-      <button onClick={handleReset} style={{ margin: 10 }}>
+      <button
+        onClick={handleReset}
+        className="btn btn-warning btn-sm"
+        style={{ margin: 10 }}
+      >
         Reset
       </button>
+
       <ResponsiveReactGridLayout
         className="layout"
         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
@@ -58,7 +78,21 @@ function App() {
             data-grid={{ x, y, w, h }}
             style={{ border: "1px solid black" }}
           >
-            <span>{i}</span>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <span>{i}</span>
+              {isEditable && (
+                <span
+                  style={{
+                    marginRight: 10,
+                    fontSize: 30,
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleRemove(i)}
+                >
+                  x
+                </span>
+              )}
+            </div>
             <Pie />
           </div>
         ))}
